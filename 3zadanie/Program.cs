daa2e457efd2ecb9203ac8553d180e3a485e166b
BeskonchinRV <BeskonchinRV@316D-5>
2022-10-15 10:09:32 +0700

    Добавьте файлы проекта.

---
+++ 3zadanie/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+while (true)
+{
+    Console.WriteLine("Введите x");
+    double x = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите  y");
+    double y = Convert.ToDouble(Console.ReadLine());
+    int R = 1;
+
+    if ((y <= 0) && (x * x + y * y <= 1))
+    {
+        Console.WriteLine("Попадание");
+    }
+    else
+    {
+        Console.WriteLine("Промах");
+    }
+
+}
